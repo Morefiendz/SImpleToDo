cdc6e3bed47219e3fae6426aeb9a0b62a4f4c8c3
X Norman <xnorman@fb.com>
2018-06-27 11:36:04 -0700

    fixed button issue

--- app/src/main/java/com/plusultra/simpletodo/MainActivity.java
+++ app/src/main/java/com/plusultra/simpletodo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.plusultra.simpletodo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,6 +11,11 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
+    //numeric code identifying edit activity
+    public static final int EDIT_REQUEST_CODE = 20;
+    //keys used for passing data between activities
+    public static final String ITEM_TEXT = "itemText";
+    public static final String ITEM_POSITION = "itemPosition";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +36,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lvItems = (ListView) findViewById(R.id.lvItems);
-        items = new ArrayList<>();
+        readItems();
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-
-        items.add("Go to Home Depot");
-        items.add("Add semicolons at the end of my lines of code");
+        //add mock items
+        //items.add("Go to Home Depot");
+        //items.add("Add semicolons at the end of my lines of code");
 
         setupListViewListener();
 
+    }
+
+    private File getDataFile() {
+        return new File(getFilesDir(), "todo.txt");
+    }
+
+    private void readItems() {
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch(IOException e) {
+            //print error to the console
+            e.printStackTrace();
+            //load an empty array list
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems(){
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            //print error to the console
+            e.printStackTrace();
+        }
     }
 
     private void setupListViewListener() {
@@ -44,8 +80,21 @@ public class MainActivity extends AppCompatActivity {
                 itemsAdapter.notifyDataSetChanged();
                 // warn user an item was deleted
                 Log.i("MainActivity", "Removed item " + i);
+                writeItems();
                 // tell framework the long click was consumed
                 return true;
+            }
+        });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent i = new Intent(MainActivity.this, EditActivity.class);
+                // put "extras" into the bundle for access in the edit activity
+                i.putExtra(ITEM_TEXT, items.get(position));
+                i.putExtra(ITEM_POSITION, position);
+                // brings up the edit activity with the expectation of a result
+                startActivityForResult(i, EDIT_REQUEST_CODE);
             }
         });
     }
@@ -57,9 +106,30 @@ public class MainActivity extends AppCompatActivity {
         String itemText = etNewItem.getText().toString();
         // add the item to the list via the adapter
         itemsAdapter.add(itemText);
+        writeItems();
         // clear the EditText by setting it to an empty String
         etNewItem.setText("");
         // display notification to user
-        Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT);
+        Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // EDIT_REQUEST_CODE defined with constants
+        if (resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE) {
+            // extract updated item value from result extras
+            String updatedItem = data.getExtras().getString(ITEM_TEXT);
+            // get the position of the item which was edited
+            int position = data.getExtras().getInt(ITEM_POSITION, 0);
+            // update the model with the new item text at the edited position
+            items.set(position, updatedItem);
+            // notify the adapter the model changed
+            itemsAdapter.notifyDataSetChanged();
+            // Store the updated items back to disk
+            writeItems();
+            // notify the user the operation completed OK
+            Toast.makeText(this, "Item updated", Toast.LENGTH_SHORT).show();
+        }
     }
 }
